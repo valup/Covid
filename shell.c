89397a059a5ca84daacdbf4ecd8d36de1ca77b3a
Valentina Prato <44057199+valup@users.noreply.github.com>
2020-12-06 18:42:20 -0300

    Add files via upload

--- shell.c
+++ shell.c
@@ -10,8 +10,6 @@
 #include "comandos.h"
 #include "straux.h"
 
-//cargar_dataset d.csv
-
 /* Imprime informacion sobre el programa */
 void help() {
   printf("\nComandos:\n\n");
@@ -51,228 +49,199 @@ y un puntero a estructuras de fechas limite, y procesa el comando
 realizando las operaciones necesarias */
 LTree procesar(Fechas* tabla, LTree lt, wchar_t* buf, struct tm** lims) {
 
-  if (!wcscoll(buf, L"help\n")) {
-    help();
-    return lt;
-  }
 
   wchar_t* pt; /* Puntero para wcstok */
   wchar_t* com = wcstok(buf, L" ", &pt); /* Toma el comando */
   wchar_t* arg = wcstok(NULL, L"\n", &pt); /* Toma los otros argumentos */
 
-  if (!arg) {
+  if (arg) {
+    if (!wcscoll(com, L"cargar_dataset")) {
+      double comienzo = clock();
+      return cargar_dataset(tabla, lt, arg, lims);
+      double final = clock();
+      printf("%f segundos\n", (final - comienzo) / CLOCKS_PER_SEC);
+
+    } else if (!wcscoll(com, L"imprimir_dataset")) {
+      double comienzo = clock();
+      imprimir_dataset(tabla, lt, arg, lims);
+      double final = clock();
+      printf("%f segundos\n", (final - comienzo) / CLOCKS_PER_SEC);
+      return lt;
+
+    } else if (!wcscoll(com, L"agregar_registro")) {
+
+      /* Separa con | los argumentos
+      (fecha departamento localidad confirmados descartados estudio)
+      y une departamento y localidad con coma */
+      wchar_t* resto = marcar_fecha(arg);
+      if (resto) {
+
+        if (marcar_notifs(resto)) {
+
+          if (marcar_lugar(resto, L'|')) {
+            /* Lee los argumentos por partes */
+            wchar_t* args[3];
+            args[0] = wcstok(arg, L"|", &pt);
+            args[1] = wcstok(NULL, L"|", &pt);
+            args[2] = wcstok(NULL, L"\n", &pt);
+
+            return agregar_registro(tabla, lt, args, lims);
+          }
+        }
+      }
+
+    } else if (!wcscoll(com, L"eliminar_registro")) {
+
+      wchar_t* resto = marcar_fecha(arg);
+      if (resto) {
+
+        if (marcar_lugar(resto, L'\0')) {
+          wchar_t* args[2];
+          args[0] = wcstok(arg, L"|", &pt);
+
+          /* Intenta convertir el primero a fecha valida */
+          struct tm* tm = string_fecha(args[0]);
+          if (tm) {
+
+            args[1] = wcstok(NULL, L"|", &pt);
+
+            eliminar_registro(tabla, tm, args[1], lims);
+            free(tm);
+            return lt;
+          }
+        }
+      }
+
+    } else if (!wcscoll(com, L"buscar_pico")) {
+      if (marcar_lugar(arg, L'\0')) {
+        buscar_pico(tabla, arg, lims);
+        return lt;
+      }
+
+    } else if (!wcscoll(com, L"casos_acumulados")) {
+
+      wchar_t* resto = marcar_fecha(arg);
+      if (resto) {
+
+        if (marcar_lugar(resto, L'\0')) {
+
+          wchar_t* args[2];
+          args[0] = wcstok(arg, L"|", &pt);
+
+          struct tm* tm = string_fecha(args[0]);
+          if (tm) {
+
+            int orden = (dias(lims[1], lims[0]) > 0);
+            if (dias(tm, lims[orden]) <= 0 && dias(lims[1 - orden], tm) <= 0) {
+
+              args[1] = wcstok(NULL, L"|", &pt);
+
+              casos_acumulados(tabla, tm, args[1]);
+
+            } else
+              printf("\nERROR: No hay registros de %d-%d-%d.\n\n",
+              tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday);
+
+            free(tm);
+            return lt;
+          }
+        }
+      }
+
+    } else if (!wcscoll(com, L"tiempo_duplicacion")) {
+
+      wchar_t* resto = marcar_fecha(arg);
+      if (resto) {
+
+        if (marcar_lugar(resto, L'\0')) {
+
+          wchar_t* args[2];
+          args[0] = wcstok(arg, L"|", &pt);
+
+          struct tm* tm = string_fecha(args[0]);
+          if (tm) {
+
+            int orden = (dias(lims[1], lims[0]) > 0);
+            if (dias(tm, lims[orden]) <= 0 && dias(lims[1 - orden], tm) <= 0) {
+              args[1] = wcstok(NULL, L"|", &pt);
+
+              int orden = (dias(lims[1], lims[0]) < 0);
+
+              tiempo_duplicacion(tabla, tm, args[1], lims[orden]);
+
+            } else
+              printf("\nERROR: No hay registros de %d-%d-%d.\n\n",
+              tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday);
+
+            free(tm);
+            return lt;
+          }
+        }
+      }
+
+    } else if (!wcscoll(com, L"graficar")) {
+
+      wchar_t* resto;
+      for (int i = 0; i < 2; i++) {
+        resto = marcar_fecha(resto);
+        if (!resto) {
+          printf("\nERROR: Faltan argumentos.\n");
+          printf("Ingrese help para mas informacion.\n\n");
+          return lt;
+        }
+      }
+
+      if (marcar_lugar(resto, L'\0')) {
+
+            wchar_t* args[3];
+            struct tm* tm[2];
+
+            for (int i = 0; i < 2; i++) {
+
+              if (i == 0) {
+                args[i] = wcstok(arg, L"|", &pt);
+              } else
+                args[i] = wcstok(NULL, L"|", &pt);
+
+              tm[i] = string_fecha(args[i]);
+              if (!tm[i]) {
+                printf("\nERROR: Faltan argumentos.\n");
+                printf("Ingrese help para mas informacion.\n\n");
+                if (i == 1)
+                  free(tm[0]);
+                return lt;
+              }
+            }
+
+            /* Confirma que la primera fecha sea anterior */
+            if (dias(tm[1], tm[0]) > 0) {
+              args[2] = wcstok(NULL, L"\n", &pt);
+              graficar(tabla, tm, args[2], lims);
+            } else
+              printf("\nERROR: Orden incorrecto de fechas.\n\n");
+
+            free(tm[0]);
+            free(tm[1]);
+            return lt;
+
+      }
+
+    } else {
+      printf("\nERROR: Comando incorrecto.\n");
+      printf("Ingrese help para mas informacion.\n\n");
+      return lt;
+    }
+  } else if (!wcscoll(buf, L"help\n")) {
+    help();
+    return lt;
+  } else {
     printf("\nERROR: Comando incorrecto.\n");
     printf("Ingrese help para mas informacion.\n\n");
     return lt;
   }
 
-  if (!wcscoll(com, L"cargar_dataset")) {
-    double comienzo = clock();
-    lt = cargar_dataset(tabla, lt, arg, lims);
-    double final = clock();
-    printf("%f segundos\n", (final - comienzo) / CLOCKS_PER_SEC);
-
-  } else if (!wcscoll(com, L"imprimir_dataset")) {
-    double comienzo = clock();
-    imprimir_dataset(tabla, lt, arg, lims);
-    double final = clock();
-    printf("%f segundos\n", (final - comienzo) / CLOCKS_PER_SEC);
-
-  } else if (!wcscoll(com, L"agregar_registro")) {
-    /* Separa con | los argumentos
-    (fecha departamento localidad confirmados descartados estudio)
-    y une departamento y localidad con coma */
-    wchar_t* resto = marcar_fecha(arg);
-    if (!resto) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    if (!marcar_notifs(resto)) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    if (!marcar_lugar(resto, L'|')) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-
-    /* Lee los argumentos por partes */
-    wchar_t* args[3];
-    args[0] = wcstok(arg, L"|", &pt);
-    args[1] = wcstok(NULL, L"|", &pt);
-    args[2] = wcstok(NULL, L"\n", &pt);
-
-    lt = agregar_registro(tabla, lt, args, lims);
-
-  } else if (!wcscoll(com, L"eliminar_registro")) {
-
-    wchar_t* resto = marcar_fecha(arg);
-    if (!resto) {
-    printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    if (!marcar_lugar(resto, L'\0')) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-
-    wchar_t* args[2];
-    args[0] = wcstok(arg, L"|", &pt);
-
-    /* Intenta convertir el primero a fecha valida */
-    struct tm* tm = string_fecha(args[0]);
-    if (!tm) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-
-    args[1] = wcstok(NULL, L"|", &pt);
-
-    eliminar_registro(tabla, tm, args[1], lims);
-    free(tm);
-
-  } else if (!wcscoll(com, L"buscar_pico")) {
-    if (!marcar_lugar(arg, L'\0')) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-    }
-
-    buscar_pico(tabla, arg, lims);
-
-  } else if (!wcscoll(com, L"casos_acumulados")) {
-
-    wchar_t* resto = marcar_fecha(arg);
-    if (!resto) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    if (!marcar_lugar(resto, L'\0')) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-
-    wchar_t* args[2];
-    args[0] = wcstok(arg, L"|", &pt);
-
-    struct tm* tm = string_fecha(args[0]);
-    if (!tm) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    } else {
-      int orden = (dias(lims[1], lims[0]) > 0);
-      if (dias(tm, lims[orden]) > 0 || dias(lims[1 - orden], tm) > 0) {
-        printf("\nERROR: No hay registros para la fecha.\n\n");
-        free(tm);
-        return lt;
-      }
-    }
-
-    args[1] = wcstok(NULL, L"|", &pt);
-
-    casos_acumulados(tabla, tm, args[1]);
-    free(tm);
-
-  } else if (!wcscoll(com, L"tiempo_duplicacion")) {
-
-    wchar_t* resto = marcar_fecha(arg);
-    if (!resto) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    if (!marcar_lugar(resto, L'\0')) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-
-    wchar_t* args[2];
-    args[0] = wcstok(arg, L"|", &pt);
-
-    struct tm* tm = string_fecha(args[0]);
-    if (!tm) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    } else {
-      int orden = (dias(lims[1], lims[0]) > 0);
-      if (dias(tm, lims[orden]) > 0 || dias(lims[1 - orden], tm) > 0) {
-        printf("\nERROR: No hay registros para la fecha.\n\n");
-        free(tm);
-        return lt;
-      }
-    }
-
-    args[1] = wcstok(NULL, L"|", &pt);
-
-    int orden = (dias(lims[1], lims[0]) < 0);
-
-    tiempo_duplicacion(tabla, tm, args[1], lims[orden]);
-
-    free(tm);
-
-  } else if (!wcscoll(com, L"graficar")) {
-
-    wchar_t* resto = marcar_fecha(arg);
-    if (!resto) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    resto = marcar_fecha(resto);
-    if (!resto) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-    if (!marcar_lugar(resto, L'\0')) {
-      printf("\nERROR: Faltan argumentos.\n");
-      printf("Ingrese help para mas informacion.\n\n");
-      return lt;
-    }
-
-    wchar_t* args[3];
-    struct tm* tm[2];
-
-    for (int i = 0; i < 2; i++) {
-    if (i == 0) {
-        args[i] = wcstok(arg, L"|", &pt);
-      } else
-        args[i] = wcstok(NULL, L"|", &pt);
-      tm[i] = string_fecha(args[i]);
-      if (!tm[i]) {
-        printf("\nERROR: Faltan argumentos.\n");
-        printf("Ingrese help para mas informacion.\n\n");
-        if (i == 1)
-          free(tm[0]);
-        return lt;
-      }
-    }
-
-    /* Confirma que la primera fecha sea anterior */
-    if (dias(tm[1], tm[0]) > 0) {
-      args[2] = wcstok(NULL, L"\n", &pt);
-      graficar(tabla, tm, args[2], lims);
-    } else
-      printf("\nERROR: Orden incorrecto de fechas.\n\n");
-
-    free(tm[0]);
-    free(tm[1]);
-
-  } else {
-    printf("\nERROR: Comando incorrecto.\n");
-    printf("Ingrese help para mas informacion.\n\n");
-  }
-
+  printf("\nERROR: Faltan argumentos.\n");
+  printf("Ingrese help para mas informacion.\n\n");
   return lt;
 }
 
